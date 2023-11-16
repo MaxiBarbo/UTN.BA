69af09fdf3d9981a5b869a7b6676d014186d14b4
Maxi.Barbo <maxi10lb@gmail.com>
2023-11-15 21:22:17 -0300

    GitHub Actions

--- ejecutar_pruebas.js
+++ ejecutar_pruebas.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 try {
   // Ejecuta los escenarios de Cucumber
-  execSync('npx cucumber-js ./features');
+  execSync('npx cucumber-js ./src/features');
 } catch (error) {
   console.error('Error al ejecutar las pruebas:', error);
   process.exit(1);
---
+++ src/features/pages/locators.js
@@ -0,0 +1,9 @@
+class Locators {
+  get btnDialogos() { return '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]'; }
+  get mensajes() { return ''; }
+  get eleccion() { return '' }
+  get contraseña() { return '' }
+  get alerta() { return '' }
+}
+
+module.exports = Locators;
